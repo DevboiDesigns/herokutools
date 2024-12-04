e5c0bc98d331550ecb635066339d2d6a10b21f2a
DevboiDesigns <75514458+DevboiDesigns@users.noreply.github.com>
2024-12-04 20:48:33 +0700

    config

--- dist/hl.script.js
+++ dist/hl.script.js
@@ -4,12 +4,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const commander_1 = require("commander");
 const child_process_1 = require("child_process");
 const program = new commander_1.Command();
-// program.version("0.0.1").description("hl command: Heroku logs")
-// console.log("process.argv", process.argv)
-if (!process.argv.slice(2).length) {
-    // If no arguments are passed to the program, output the help
-    program.outputHelp();
-}
+// If no arguments are passed to the program, output the help
+// if (!process.argv.slice(2).length) {
+//   program.outputHelp()
+// }
 program
     .version("0.0.1")
     .description("Heroku logs: wrapper")
@@ -19,10 +17,13 @@ program
     // .option("-t, --touch <value>", "Create a file")
     .parse(process.argv);
 const options = program.opts();
-if (options.app) {
-    herokuLogs(options.app);
-}
+// if (options.app) {
+herokuLogs(options.app);
+// }
 function herokuLogs(appName) {
+    if (!appName) {
+        appName = "stocklift-dev";
+    }
     console.log("herokuLogs for:", appName);
     const child = (0, child_process_1.spawnSync)(`heroku logs -a ${appName} --tail`, {
         shell: true,
--- src/hl.script.ts
+++ src/hl.script.ts
@@ -4,14 +4,10 @@ import { Command } from "commander"
 import { spawnSync } from "child_process"
 const program = new Command()
 
-// program.version("0.0.1").description("hl command: Heroku logs")
-
-// console.log("process.argv", process.argv)
-
-if (!process.argv.slice(2).length) {
-  // If no arguments are passed to the program, output the help
-  program.outputHelp()
-}
+// If no arguments are passed to the program, output the help
+// if (!process.argv.slice(2).length) {
+//   program.outputHelp()
+// }
 
 program
   .version("0.0.1")
@@ -24,11 +20,14 @@ program
 
 const options = program.opts()
 
-if (options.app) {
-  herokuLogs(options.app)
-}
+// if (options.app) {
+herokuLogs(options.app)
+// }
 
 function herokuLogs(appName: string) {
+  if (!appName) {
+    appName = "stocklift-dev"
+  }
   console.log("herokuLogs for:", appName)
 
   const child = spawnSync(`heroku logs -a ${appName} --tail`, {
