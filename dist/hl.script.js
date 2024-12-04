b92c1a40958397ed1f40cef2a10d8f39652f9744
DevboiDesigns <75514458+DevboiDesigns@users.noreply.github.com>
2024-12-04 20:58:50 +0700

    config

--- dist/hl.script.js
+++ dist/hl.script.js
@@ -1,36 +1,34 @@
 #! /usr/bin/env node
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const commander_1 = require("commander");
 const child_process_1 = require("child_process");
+const commander_1 = require("commander");
 const program = new commander_1.Command();
 // If no arguments are passed to the program, output the help
 // if (!process.argv.slice(2).length) {
 //   program.outputHelp()
 // }
-program
-    .version("0.0.1")
-    .description("Heroku logs: wrapper")
-    // .option("-a, --app <type>", "App name")
-    // .option("-t, --tail <boolean>", "Show logs in real time")
-    .option("-a, --app <app name>", "Logs for an app")
-    // .option("-t, --touch <value>", "Create a file")
-    .parse(process.argv);
-const options = program.opts();
-// if (options.app) {
-herokuLogs(options.app);
-// }
-function herokuLogs(appName) {
+const herokuLogs = (appName) => {
     if (!appName) {
         appName = "stocklift-dev";
     }
-    console.log("herokuLogs for:", appName);
+    console.log(`Fetching logs for ${appName}`);
     const child = (0, child_process_1.spawnSync)(`heroku logs -a ${appName} --tail`, {
         shell: true,
         stdio: "inherit",
     });
-    // console.log("error", child.error)
-    console.log("stdout ", child.stdout);
-    // console.log("stderr ", child.stderr)
-}
+    if (child.error) {
+        console.log(child.error);
+    }
+    else {
+        console.log(child.stdout);
+    }
+};
+program
+    .version("0.0.1")
+    .description("Heroku logs: cli wrapper")
+    .option("-a, --app <app name>", "Logs for your Heroku app, defaults to the saved app name")
+    .parse(process.argv);
+const options = program.opts();
+herokuLogs(options.app);
 //# sourceMappingURL=hl.script.js.map
--- src/hl.script.ts
+++ src/hl.script.ts
@@ -1,7 +1,7 @@
 #! /usr/bin/env node
 
-import { Command } from "commander"
 import { spawnSync } from "child_process"
+import { Command } from "commander"
 const program = new Command()
 
 // If no arguments are passed to the program, output the help
@@ -9,33 +9,32 @@ const program = new Command()
 //   program.outputHelp()
 // }
 
-program
-  .version("0.0.1")
-  .description("Heroku logs: wrapper")
-  // .option("-a, --app <type>", "App name")
-  // .option("-t, --tail <boolean>", "Show logs in real time")
-  .option("-a, --app <app name>", "Logs for an app")
-  // .option("-t, --touch <value>", "Create a file")
-  .parse(process.argv)
-
-const options = program.opts()
-
-// if (options.app) {
-herokuLogs(options.app)
-// }
-
-function herokuLogs(appName: string) {
+const herokuLogs = (appName: string) => {
   if (!appName) {
     appName = "stocklift-dev"
   }
-  console.log("herokuLogs for:", appName)
-
+  console.log(`Fetching logs for ${appName}`)
   const child = spawnSync(`heroku logs -a ${appName} --tail`, {
     shell: true,
     stdio: "inherit",
   })
 
-  // console.log("error", child.error)
-  console.log("stdout ", child.stdout)
-  // console.log("stderr ", child.stderr)
+  if (child.error) {
+    console.log(child.error)
+  } else {
+    console.log(child.stdout)
+  }
 }
+
+program
+  .version("0.0.1")
+  .description("Heroku logs: cli wrapper")
+  .option(
+    "-a, --app <app name>",
+    "Logs for your Heroku app, defaults to the saved app name"
+  )
+  .parse(process.argv)
+
+const options = program.opts()
+
+herokuLogs(options.app)
